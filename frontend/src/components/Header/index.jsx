9f85e566fe2adbfc8a0994db8fd2a1153d972037
wallace027dev <wallaceofc@hotmail.com>
2025-01-10 10:21:29 -0300

    projeto para aula

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import arrowDown from "../../assets/arrow-down.svg";
 import mixer from "../../assets/mixer.svg"
-// Properties
+
 export default function Header(props) {
   return (
     <header>
         <button type="button">
           <img src={arrowDown} alt="Arrow down icon" />
         </button>
-        <span>{props.title}</span>
+        <h1>{props.title}</h1>
         <button type="button">
           <img src={mixer} alt="Mixer icon" />
         </button>
